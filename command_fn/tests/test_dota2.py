dafe57a93c1940c0bc03391d5cf378d2337ce9c5
ghosalya <12974269+ghosalya@users.noreply.github.com>
2023-05-06 09:06:51 +0800

    fix dota get_single_draft test after new hero category

--- command_fn/tests/test_dota2.py
+++ command_fn/tests/test_dota2.py
@@ -4,4 +4,4 @@ from command_fn.dota2 import get_single_draft
 def test_single_draft():
     draft = get_single_draft()
     print(draft)
-    assert len(draft.split("\n")) == 4
+    assert len(draft.split("\n")) == 5
